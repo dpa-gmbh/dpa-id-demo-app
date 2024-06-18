7f085bb7a4d2360e4822c73d9187b7ff7373bd87
Roy, Mridul <roy.mridul@dpa.com>
2024-06-18 16:41:09 +0200

    remove param from route

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,9 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+onMounted(() => router.push('/'))
 </script>
